686f0fb7408ab62a36227a44859eab53284f7bf0
Srijoy Paul <srijoykumar1507@gmail.com>
2023-06-24 12:47:25 +0530

    Bounce ball off the walls

--- scripts.js
+++ scripts.js
@@ -3,12 +3,12 @@ const context = game_canvas.getContext("2d");
 
 let x = game_canvas.width / 2;
 let y = game_canvas.height - 30;
-let dx = -2, dy = -2;
-console.log(x, y);
+let dx = 2, dy = -2;
+const ballRadius = 5;
 
 function drawBall() {
     context.beginPath();
-    context.arc(x, y, 10, 0, Math.PI * 2);
+    context.arc(x, y, ballRadius, 0, Math.PI * 2);
     context.fillStyle = "blue";
     context.fill();
     context.closePath();
@@ -17,8 +17,12 @@ function drawBall() {
 function draw() {
     context.clearRect(0, 0, game_canvas.width, game_canvas.height);
     drawBall();
+    if (x + dx > game_canvas.width - ballRadius || x + dx < ballRadius)
+        dx = -dx;
+    if (y + dy > game_canvas.height - ballRadius || y + dy < ballRadius)
+        dy = -dy;
     x += dx;
     y += dy;
 }
 
-setInterval(draw, 15);
+setInterval(draw, 17);
