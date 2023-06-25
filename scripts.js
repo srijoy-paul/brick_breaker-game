98185fcd7da05fa78533fd58033b9cd14ed17881
Srijoy Paul <srijoykumar1507@gmail.com>
2023-06-26 01:17:05 +0530

    Brick Fields collision detection

--- scripts.js
+++ scripts.js
@@ -24,14 +24,46 @@ const bricks = [];
 for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for (let r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight")
+        rightPressed = true;
+    else if (e.key === "Left" || e.key === "ArrowLeft")
+        leftPressed = true;
+}
+
+function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight")
+        rightPressed = false;
+    else if (e.key === "Left" || e.key === "ArrowLeft")
+        leftPressed = false;
+}
+let colorChange = false;
+function collisionDetection() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            const b = bricks[c][r];
+            if (b.status === 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    colorChange = true;
+                }
+            }
+        }
     }
 }
 
 function drawBall() {
     context.beginPath();
     context.arc(x, y, ballRadius, 0, Math.PI * 2);
-    context.fillStyle = "blue";
+    if (colorChange) context.fillStyle = "#DD58D6";
+    else context.fillStyle = "blue";
     context.fill();
     context.closePath();
 }
@@ -47,15 +79,17 @@ function drawPaddle() {
 function drawBricks() {
     for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickRowCount; r++) {
-            const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-            const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            context.beginPath();
-            context.rect(brickX, brickY, brickWidth, brickHeight);
-            context.fillStyle = "";
-            context.fill();
-            context.closePath();
+            if (bricks[c][r].status === 1) {
+                const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                context.beginPath();
+                context.rect(brickX, brickY, brickWidth, brickHeight);
+                context.fillStyle = "";
+                context.fill();
+                context.closePath();
+            }
         }
     }
 }
@@ -65,6 +99,7 @@ function draw() {
     drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
     if (x + dx > game_canvas.width - ballRadius || x + dx < ballRadius)
         dx = -dx;
     if (y + dy < ballRadius)
@@ -93,20 +128,4 @@ function draw() {
     }
 }
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-function keyDownHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight")
-        rightPressed = true;
-    else if (e.key === "Left" || e.key === "ArrowLeft")
-        leftPressed = true;
-}
-
-function keyUpHandler(e) {
-    if (e.key === "Right" || e.key === "ArrowRight")
-        rightPressed = false;
-    else if (e.key === "Left" || e.key === "ArrowLeft")
-        leftPressed = false;
-}
 const interval = setInterval(draw, 18);
